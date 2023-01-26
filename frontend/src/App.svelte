b1f86b3d93823ccc6d924c26230f8fbebe81b27c
nicecarrot <nicecarrot20@gmail.com>
2023-01-26 19:00:03 +0900

    "Docs:README Update" 사다리게임을 사다리타기로 수정

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -6,23 +6,13 @@
   <div>
     
   </div>
-  <h1>공정한 사다리게임</h1>
+  <h1>공정한 사다리타기</h1>
 
   <div class="card">
     <Counter />
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .read-the-docs {
-    color: #888;
-  }
 </style>
